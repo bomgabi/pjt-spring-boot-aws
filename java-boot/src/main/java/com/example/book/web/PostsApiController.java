1803b0a45b48775fc28af5b06d2cd5c1ae3d45ae
“danny” <gryb809@gmail.com>
2022-03-05 10:44:54 +0900

    :sparkles: Posts 등록 및 단위테스트 개발 완료

---
+++ java-boot/src/main/java/com/example/book/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.example.book.web;
+
+import com.example.book.service.PostsService;
+import com.example.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+// TODO 왜 생성자 주입을 권장하는가?
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
