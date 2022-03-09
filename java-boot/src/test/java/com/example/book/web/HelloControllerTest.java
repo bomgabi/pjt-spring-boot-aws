d828ffb6a0c90387bd5191fc496b8c12905571d7
danny <gryb809@gmail.com>
2022-03-10 00:08:46 +0900

    :white_check_mark: Controller 및 lombok 설정 테스트 추가

---
+++ java-boot/src/test/java/com/example/book/web/HelloControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.book.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Hello 리턴")
+    @WithMockUser(roles = "USER")
+    public void hello() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    @DisplayName("helloDto 리턴")
+    @WithMockUser(roles = "USER")
+    public void helloDto() throws Exception {
+        String name = "hello";
+        int amount = 100;
+
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
---
+++ java-boot/src/test/java/com/example/book/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.example.book.web.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    @DisplayName("lombok 테스트")
+    public void lombok() {
+        //given
+        String name = "test";
+        int amount = 120;
+
+        //when
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(helloResponseDto.getName()).isEqualTo(name);
+        assertThat(helloResponseDto.getAmount()).isEqualTo(amount);
+    }
+}
